b7ced1b770e745a499790a24fd1e7f7b6f24a091
Ameya1605 <146834099+Ameya1605@users.noreply.github.com>
2026-03-05 15:33:50 +0530

    fix render port

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,7 @@ const fs = require("fs");
 const nodemailer = require("nodemailer");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
